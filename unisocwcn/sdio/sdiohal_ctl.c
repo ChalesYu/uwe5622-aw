1df23b78e39b65b952e90cce6e363e06c32d4e37
chalesyu <574249312@qq.com>
2021-06-05 11:45:08 +0800

    Revert "unisocwcn/sdio/sdiohal_ctl.c : use ktime_get() replace do_gettimeofday()"
    This reverts commit 34aa4889158e7ccb1d82437d1f4c97ea48dc7117.

--- unisocwcn/sdio/sdiohal_ctl.c
+++ unisocwcn/sdio/sdiohal_ctl.c
@@ -96,7 +96,8 @@ char *tp_tx_buf[TP_TX_BUF_CNT];
 
 struct mchn_ops_t at_tx_ops;
 struct mchn_ops_t at_rx_ops;
-ktime_t tp_tx_start_time, tp_tx_stop_time;
+struct timeval tp_tx_start_time;
+struct timeval tp_tx_stop_time;
 int tp_tx_cnt;
 int tp_tx_flag;
 int tp_tx_buf_cnt = TP_TX_BUF_CNT;
@@ -220,7 +221,7 @@ static int sdiohal_throughput_tx(void)
 
 static void sdiohal_throughput_tx_compute_time(void)
 {
-	static u64 times_count;
+	static signed long long times_count;
 
 	if (tp_tx_flag != 1)
 		return;
@@ -228,12 +229,13 @@ static void sdiohal_throughput_tx_compute_time(void)
 	/* throughput test */
 	tp_tx_cnt++;
 	if (tp_tx_cnt % 500 == 0) {
-		tp_tx_stop_time = ktime_get();
-		times_count = ktime_us_delta(tp_tx_stop_time, tp_tx_start_time);
+		do_gettimeofday(&tp_tx_stop_time);
+		times_count = timeval_to_ns(&tp_tx_stop_time) -
+			timeval_to_ns(&tp_tx_start_time);
 		sdiohal_info("tx->times(500c) is %lldns, tx %d, rx %d\n",
 			     times_count, tp_tx_cnt, rx_pop_cnt);
 		tp_tx_cnt = 0;
-		tp_tx_start_time = ktime_get();
+		do_gettimeofday(&tp_tx_start_time);
 	}
 	sdiohal_throughput_tx();
 }
@@ -537,12 +539,13 @@ int at_list_tx_pop(int channel, struct mbuf_t *head,
 }
 
 int tp_rx_cnt;
-ktime_t tp_rx_start_time, tp_rx_stop_time;
+struct timeval tp_rx_start_time;
+struct timeval tp_rx_stop_time;
 struct timespec tp_tm_begin;
 int at_list_rx_pop(int channel, struct mbuf_t *head,
 		   struct mbuf_t *tail, int num)
 {
-	static u64 times_count;
+	static signed long long times_count;
 
 	sdiohal_debug("%s channel:%d head:%p tail:%p num:%d\n",
 		     __func__, channel, head, tail, num);
@@ -559,12 +562,13 @@ int at_list_rx_pop(int channel, struct mbuf_t *head,
 	/* throughput test */
 	tp_rx_cnt += num;
 	if (tp_rx_cnt / (500*64) == 1) {
-		tp_rx_stop_time = ktime_get();
-		times_count = ktime_us_delta(tp_rx_stop_time, tp_rx_start_time);
+		do_gettimeofday(&tp_rx_stop_time);
+		times_count = timeval_to_ns(&tp_rx_stop_time)
+			- timeval_to_ns(&tp_rx_start_time);
 		sdiohal_info("rx->times(%dc) is %lldns, tx %d, rx %d\n",
 			     tp_rx_cnt, times_count, tp_tx_cnt, rx_pop_cnt);
 		tp_rx_cnt = 0;
-		tp_rx_start_time = ktime_get();
+		do_gettimeofday(&tp_rx_start_time);
 	}
 	getnstimeofday(&tp_tm_begin);
 
@@ -1112,7 +1116,7 @@ static ssize_t at_cmd_write(struct file *filp,
 			__func__, tp_tx_buf_cnt, tp_tx_buf_len);
 		tp_tx_flag = 1;
 		tp_tx_cnt = 0;
-		tp_tx_start_time = ktime_get();
+		do_gettimeofday(&tp_tx_start_time);
 		if ((tp_tx_buf_cnt <= TP_TX_BUF_CNT) &&
 			(tp_tx_buf_len <= TP_TX_BUF_LEN)) {
 			sprdwcn_bus_chn_deinit(&at_tx_ops);
