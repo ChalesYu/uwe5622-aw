f53d7cdd535aadaa32400878443f30df4b83de9a
chalesyu <574249312@qq.com>
2021-06-06 08:56:35 +0800

    unisocwcn/platform/rdc_debug.c: change config/firmware/debug file path

--- unisocwcn/platform/rdc_debug.c
+++ unisocwcn/platform/rdc_debug.c
@@ -41,14 +41,16 @@ static unsigned int wcn_cp2_file_max_num = UNISOC_DBG_FILENUM_DEFAULT;
  */
 static unsigned int wcn_cp2_log_cover_old = 1;
 /* path of config file unisoc_cp2log_config.txt */
-#define WCN_DEBUG_CFG_MAX_PATH_NUM	2
+#define WCN_DEBUG_CFG_MAX_PATH_NUM	3
 static char *wcn_cp2_config_path[WCN_DEBUG_CFG_MAX_PATH_NUM] = {
+	"/etc/unisoc_cp2log_config.txt",
 	"/data/unisoc_cp2log_config.txt",
 	"/vendor/etc/wifi/unisoc_cp2log_config.txt"
 };
 /* path of cp2 log and mem files. */
-#define WCN_UNISOC_DBG_MAX_PATH_NUM	3
+#define WCN_UNISOC_DBG_MAX_PATH_NUM	4
 static char *wcn_unisoc_dbg_path[WCN_UNISOC_DBG_MAX_PATH_NUM] = {
+	"/etc",
 	UNISOC_DBG_PATH_DEFAULT,/* most of projects */
 	"/data",		/* amlogic s905w... */
 	"/mnt/UDISK"		/* allwinner r328... */
